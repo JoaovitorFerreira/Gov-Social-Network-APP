3ec6ca127b3ce557cfe6ddd9f79e0c9a952defae
Matheus Moreira <matheus.c.g.moreira@gmail.com>
2022-05-24 15:26:55 -0300

    deploy config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBdSAiLnKVj_1kGKsWIrSys4HelPSDhBDI',
+    authDomain: 'pge-knowledge-environment.firebaseapp.com',
+    projectId: 'pge-knowledge-environment',
+    storageBucket: 'pge-knowledge-environment.appspot.com',
+    messagingSenderId: '946785114191',
+    appId: '1:946785114191:web:4aa87793d448aee37a323a'
+  }
 };
